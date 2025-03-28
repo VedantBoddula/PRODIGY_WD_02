14bd5a8573f58ee26f7039bbfae291b19bace311
vedant <vedantboddula9860gmail.com>
2025-03-29 01:25:17 +0530

    jsddhs

--- stopwatch.js
+++ stopwatch.js
@@ -8,6 +8,9 @@ const minute=document.body.querySelector("#minute")
 const startButton=document.body.querySelector(".start-button")
 const resetButton=document.body.querySelector(".reset-button")
 const stopButton=document.body.querySelector(".stop-button")
+const lapButton=document.body.querySelector(".lap-button")
+
+const lapReset = document.querySelector(".reset-img");
 
 
 setInterval(increment, 1000);
@@ -43,14 +46,65 @@ startButton.addEventListener("click",()=>{
 })
 
 resetButton.addEventListener("click",()=>{
+    // createLapBox(hours,minutes,seconds);
     seconds=-1;
     minutes=0;
     hours=0;
 })
 
+function createLapBox(hrs,mins,sec){
+    const laps = document.getElementById("laps");
+    const box = document.createElement("div");
 
-const element = document.getElementById("center");
-  element.style.position = "absolute";
-  element.style.top = "50%";
-  element.style.left = "50%";
-  element.style.transform = "translate(-50%, -50%)";
+    box.classList.add("lapkoremove"); //
+    
+    const hrBox = document.createElement("span");
+    const minBox = document.createElement("span");
+    const seBox = document.createElement("span");
+
+       
+
+    hrBox.innerText = hrs;
+    minBox.innerText = ":"+mins;
+    seBox.innerText = ":"+sec;
+    box.appendChild(hrBox)
+    box.appendChild(minBox)
+    box.appendChild(seBox)
+    laps.appendChild(box)
+    box.classList.add("move")
+    
+    // first_page.classList.add("hidden")
+    // second_page.classList.remove("hidden")
+}
+
+lapButton.addEventListener("click",()=>{createLapBox(hours,minutes,seconds)})
+
+function removeLaps(){
+    const reemove=document.querySelectorAll(".lapkoremove")
+    const lapss = document.getElementById("laps");
+
+    for(let i=0;i<reemove.length;i++){
+        console.log(reemove[i])
+        lapss.removeChild(reemove[i])
+    }
+    console.log('tytsjks');
+    
+}
+
+
+lapReset.addEventListener("click",removeLaps)
+    // box.appendChild(hrBox)
+    // box.appendChild(minBox)
+    // box.appendChild(seBox)
+
+   
+    // box.classList.add("move")
+    
+
+
+
+
+
+
+
+
